3edcecf1ee288805bcf854cb9fc3027defd2dec9
Shimanto <shimanto7710@gmail.com>
2026-05-08 00:15:02 +0200

    Initial Commit

---
+++ BahnNavigator/BahnNavigatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  BahnNavigatorApp.swift
+//  BahnNavigator
+//
+//  Created by Shimanto A. on 8/5/26.
+//
+
+import SwiftUI
+
+@main
+struct BahnNavigatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BahnNavigatorTests/BahnNavigatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  BahnNavigatorTests.swift
+//  BahnNavigatorTests
+//
+//  Created by Shimanto A. on 8/5/26.
+//
+
+import Testing
+@testable import BahnNavigator
+
+struct BahnNavigatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
